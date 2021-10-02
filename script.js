8939c76782f87204c64901a61c4a0d4175eb793c
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2021-10-02 12:23:55 -0300

    adicionado o movimento para o cactus

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ function jump() {
     let upInterval = setInterval(() => {
         if (position >= 150) {
             clearInterval(upInterval);
+
+            //descendo
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval)
@@ -32,6 +34,8 @@ function jump() {
                 }
             }, 30);
         } else {
+
+            //subindo
             position += 20;
             dino.style.bottom = `${position}px`
         }
@@ -41,10 +45,25 @@ function jump() {
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = `${cactusPosition}px`
     background.appendChild(cactus);
+
+    //movimento do cactus
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = `${cactusPosition}px`;
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
+
 createCactus();
 document.addEventListener('keydown', handleKeyUp);
